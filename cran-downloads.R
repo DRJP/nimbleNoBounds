41a1acac45bd0a6aaea36566e6806c32665ac97c
David Pleydell <david.pleydell@inrae.fr>
2022-05-25 14:12:47 +0200

    Edits in cran-downloads.R

--- cran-downloads.R
+++ cran-downloads.R
@@ -5,17 +5,17 @@ library(dplyr)
 library(tibble)
 
 # Last week's downloads
-cran_downloads(packages="nimbleChangeOfVariables", when="last-week")
+cran_downloads(packages="nimbleNoBounds", when="last-week")
 
 
 # How many overall downloads
-mls <- cran_downloads(packages="nimbleChangeOfVariables", from = "2021-11-11", to = Sys.Date()-1)
+mls <- cran_downloads(packages="nimbleNoBounds", from = "2022-05-23", to = Sys.Date()-1)
 mls
 sum(mls[,2])
 
 # Plot
 gr0 <- ggplot(mls, aes(date, count)) + geom_line(colour = "red",size=1)
-gr0 + xlab("Time") + ylab("Nr. of downloads") + labs(title = paste0("nimbleChangeOfVariables daily downloads ", Sys.Date()-1))
+gr0 + xlab("Time") + ylab("Nr. of downloads") + labs(title = paste0("nimbleNoBounds daily downloads ", Sys.Date()-1))
 
 # Cumulate downloads
 cumulative <- cumsum(mls[,2])
@@ -26,4 +26,4 @@ tibble(mls2) %>% filter(count>0) %>% select(date) %>% tail(n=1) %>% pull() -> tS
 # Plot
 gr1 <- ggplot(mls2, aes(date, cumulative)) + geom_line(colour = "blue",size=1)
 gr1 + xlab("Time") + ylab("Cumulative downloads") +
-  labs(title = paste0("nimbleChangeOfVariables downloads: ", tStart, " to ", tStop))
+  labs(title = paste0("nimbleNoBounds downloads: ", tStart, " to ", tStop))
